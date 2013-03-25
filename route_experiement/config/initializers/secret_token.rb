559285b64ded6970e2a94488a7355d41697d6619
John Maddux <jemaddux@gmail.com>
2013-03-25 11:27:36 -0600

    Routing testing app

---
+++ route_experiement/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RouteExperiement::Application.config.secret_token = 'd0738d5eb1c666aa97c2df08015461cc63c40186c0c35c61827ab28f9b8a8d605f8be4284767b9a60e0bd0856d3961a6f96b1b5e89b68f0554a373ebe8a9fc71'
