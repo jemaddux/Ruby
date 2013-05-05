01c00dd9fd7ed84a20a00446d8a2606c0efa02ba
John Maddux <jemaddux@gmail.com>
2013-05-05 15:46:48 -0600

    small fix

--- even.rb
+++ even.rb
@@ -1,10 +1,10 @@
 class Even
   def self.how_many_evens_upto(number)
-    evens = (0..number).each_with_object([]) do |num, evens|
+    evens = (2..number).each_with_object([]) do |num, evens|
       evens << num if num % 2 == 0
     end
     puts evens.count
   end
 end
 
-Even.how_many_evens_upto(1_000)
+Even.how_many_evens_upto(4)
