91d587c319de1c28eab6c24ff4a44a3c71a022e5
John Maddux <jemaddux@gmail.com>
2013-08-31 09:46:49 -0400

    Added more files

---
+++ Rails/cyoa_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CyoaApp::Application.config.secret_token = '6d906b52dff69c8bba9c86515c9f359827f9794a3481d4fd6de3351ffbaedb58907f891d149d725f09998e4f131236634483ab271a7aefcfefbc78773fe0d781'
