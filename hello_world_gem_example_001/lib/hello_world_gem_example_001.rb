33e9c867f8a32400cd14785d619b2d7275929990
John Maddux <jemaddux@gmail.com>
2014-06-08 14:15:25 -0400

    ...

---
+++ hello_world_gem_example_001/lib/hello_world_gem_example_001.rb
@@ -0,0 +1,5 @@
+require "hello_world_gem_example_001/version"
+
+module HelloWorldGemExample001
+  # Your code goes here...
+end
---
+++ hello_world_gem_example_001/lib/hello_world_gem_example_001/version.rb
@@ -0,0 +1,3 @@
+module HelloWorldGemExample001
+  VERSION = "0.0.1"
+end
